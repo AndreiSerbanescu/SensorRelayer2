14081302b4974d3b5a57018cf41867646de6abe5
Andrei Serbanescu <andrei.serbanescu16@imperial.ac.uk>
2018-11-26 01:03:57 +0000

    animations disappear on touch, fixed position and crashing bug

--- app/src/main/java/com/android/developer/blechat/DebugActivity.java
+++ app/src/main/java/com/android/developer/blechat/DebugActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieComposition;
 
 public class DebugActivity extends AppCompatActivity {
 
@@ -21,34 +22,28 @@ public class DebugActivity extends AppCompatActivity {
         setupComponents();
     }
 
+//    private void setupComponents() {
+//        Button waitingButton = findViewById(R.id.waiting_button);
+//        Button lightSensorButton = findViewById(R.id.light_sensor_button);
+//
+//        LottieAnimationView loadingAV = initAnimation(R.id.loading_animation);
+//        LottieAnimationView lightSensorAV = initAnimation(R.id.light_sensor_animation);
+//
+//        loadingAV.setVisibility(View.VISIBLE);
+//        loadingAV.playAnimation();
+//
+//        waitingButton.setOnClickListener(v -> handleWaitingButtonOnClick(loadingAV));
+//        lightSensorButton.setOnClickListener(v -> handleLightButtonClick(lightSensorAV));
+//    }
+
     private void setupComponents() {
         Button waitingButton = findViewById(R.id.waiting_button);
         Button lightSensorButton = findViewById(R.id.light_sensor_button);
 
-        LottieAnimationView loadingAV = initAnimation(R.id.loading_animation);
-        LottieAnimationView lightSensorAV = initAnimation(R.id.light_sensor_animation);
 
-        loadingAV.addAnimatorListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-                loadingAV.setVisibility(View.VISIBLE);
-            }
+        LottieAnimationView lightSensorAV = findViewById(R.id.light_sensor_animation);
+        lightSensorAV.setVisibility(View.INVISIBLE);
 
-            @Override
-            public void onAnimationEnd(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-                loadingAV.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-        });
         lightSensorAV.addAnimatorListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animation) {
@@ -62,7 +57,7 @@ public class DebugActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationCancel(Animator animation) {
-
+                lightSensorAV.setVisibility(View.GONE);
             }
 
             @Override
@@ -71,15 +66,50 @@ public class DebugActivity extends AppCompatActivity {
             }
         });
 
-        waitingButton.setOnClickListener(v -> handleWaitingButtonOnClick(loadingAV));
-        lightSensorButton.setOnClickListener(v -> handleLightButtonClick(lightSensorAV));
+        lightSensorAV.setOnClickListener(v -> lightSensorAV.cancelAnimation());
+
+        lightSensorButton.setOnClickListener(v -> lightSensorAV.playAnimation());
+
+
+        LottieAnimationView loadingAnimation = findViewById(R.id.loading_animation);
+        loadingAnimation.setVisibility(View.INVISIBLE);
+
+        loadingAnimation.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                loadingAnimation.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                loadingAnimation.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+        loadingAnimation.setOnClickListener(v -> loadingAnimation.cancelAnimation());
+        waitingButton.setOnClickListener(v -> loadingAnimation.playAnimation());
     }
 
     private void handleWaitingButtonOnClick(LottieAnimationView loadingAnimationView) {
-        loadingAnimationView.playAnimation();
+        //loadingAnimationView.setVisibility(View.VISIBLE);
+        //loadingAnimationView.playAnimation();
+
+        loadingAnimationView.cancelAnimation();
+        loadingAnimationView.setVisibility(View.INVISIBLE);
     }
 
     private void handleLightButtonClick(LottieAnimationView lightSensorAV) {
+        lightSensorAV.setVisibility(View.VISIBLE);
         lightSensorAV.playAnimation();
     }
 
